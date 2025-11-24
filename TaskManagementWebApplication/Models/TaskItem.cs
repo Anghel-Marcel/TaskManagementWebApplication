27636c8b5723ee7c78a33d7747e70bc638f408b0
Anghel-Marcel <anghel.marcel.t8t@student.ucv.ro>
2025-11-24 21:22:16 +0200

    Add AddTaskItemTable Migration

---
+++ TaskManagementWebApplication/Models/TaskItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementWebApplication.Models
+{
+    public class TaskItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = "ToDo";
+
+        // FK to Identity user
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual Microsoft.AspNetCore.Identity.IdentityUser User { get; set; }
+    }
+}
